811a8682315ac0578387fbce296e3e8fd1c26199
WhatsApp API for developers <care@whapi.cloud>
2023-10-31 21:40:21 +0300

    whatsapp api bot main

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // API endpoint URL
+    apiUrl: "https://gate.whapi.cloud",
+    // API token from your panel
+    token: "HLH3FdPYS3s6iU9REgagm5D2rJ9edUp1 ",
+    // Bot`s URL (for static file)
+    group: '120363177599599603@g.us',
+    product: '6559353560856703',
+    botUrl: null
+}
