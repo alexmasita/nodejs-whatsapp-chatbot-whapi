19f6c48af3e57310f6ca992a2f8a2ebde7f86f34
WhatsApp API for developers <care@whapi.cloud>
2023-11-10 20:32:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 module.exports = {
     // API endpoint URL
     apiUrl: "https://gate.whapi.cloud",
-    // API token from your panel
-    token: "HL6iU9dURYgmD23FaedrJ5EHP9S3sgp1 ",
-    group: '120363177599599603@g.us',
+    // API token from your channel
+    token: "YOUR CHANNEL TOKEN",
+// The ID of the group to which we will send the message. Use to find out the ID: https://whapi.readme.io/reference/getgroups
+    group: '120363167596599603@g.us',
+// The ID of the product we will send for the example. Create a product in your WhatsApp and find out the product ID: https://whapi.readme.io/reference/getproducts
     product: '6559353560856703',
-    // Bot`s URL (for static file)
-    botUrl: null
+    // Bot`s URL (for static file). Webhook Link to your server. At ( {server link}/messages ), when POST is requested, processing occurs in index.php
+    botUrl: "https://yoursite.com/messages"
 }
